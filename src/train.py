f0344174fa74c2f7fe166123f2dcaafaca6f349d
Artificial-Git <99414315+Artificial-Git@users.noreply.github.com>
2022-03-31 17:21:28 +0200

    DIG-382 Modell in github action trainieren und hochladen

--- src/train.py
+++ src/train.py
@@ -16,7 +16,8 @@ import pickle
 # name=True bedeutet, dass Header als solche gespeichert werden.
 # auto_array[i] gibt alle Attribute der iten Instanz, auto_array["attribut"] gibt "attribut" aller Instanzen raus.
 #auto_array = np.genfromtxt('../data/auto-mpg.csv', dtype=float, delimiter=";", names=True)
-data = pd.read_csv('../data/auto-mpg.csv', sep=";")
+#data = pd.read_csv('../data/auto-mpg.csv', sep=";")
+data = pd.read_csv('./auto-mpg.csv', sep=";")
 
 print(data)
 
@@ -36,7 +37,7 @@ regressor = LinearRegression()
 
 regressor = regressor.fit(x_train, y_train)
 
-#Vorhersage testen.
+# Vorhersage testen.
 y_pred = regressor.predict(x_test)
 
 # wb: öffnen für writing und im Binärmodus
