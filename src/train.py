fe80bc1a9123f6f4653d11dc0f1581fafdb22ed5
Artificial-Git <starace@zedat.fu-berlin.de>
2022-04-03 22:32:26 +0200

    DIG-383 Automatisiertest pytest Testing aufsetzen

--- src/train.py
+++ src/train.py
@@ -1,5 +1,4 @@
-import sklearn
-#from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 #from sklearn.preprocessing import MinMaxScaler
 #from sklearn.preprocessing import PolynomialFeatures
