2d68bcb59c0c939b989029c23392ae06e5d42d25
Artificial-Git <starace@zedat.fu-berlin.de>
2022-04-03 21:45:34 +0200

    DIG-383 Automatisiertest pytest Testing aufsetzen

--- src/predict.py
+++ src/predict.py
@@ -2,12 +2,14 @@ import pandas as pd
 import pickle
 
 # Trainiertes Modell laden. 'rb': read Binärdatei
-file_to_open = open("../data/models/baummethoden_lr.pickle", 'rb')
+#file_to_open = open("../data/models/baummethoden_lr.pickle", 'rb')
+file_to_open = open("data/models/baummethoden_lr.pickle", 'rb')
 trained_model = pickle.load(file_to_open)
 file_to_open.close()
 
 # Lade Daten, für die wir Vorhersagen wollen
-prediction_data = pd.read_csv('../data/prediction-data.csv', sep=";")
+#prediction_data = pd.read_csv('../data/prediction-data.csv', sep=";")
+prediction_data = pd.read_csv('data/prediction-data.csv', sep=";")
 
 #print(trained_model.predict(prediction_data.loc[:, prediction_data.columns != 'mpg']))
 
