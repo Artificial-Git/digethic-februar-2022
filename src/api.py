19ef398487438cfe718da061bd626f06a62b7c15
Artificial-Git <99414315+Artificial-Git@users.noreply.github.com>
2022-04-06 17:18:18 +0200

    DIG-408: MS Erstellung einer flask-API

--- src/api.py
+++ src/api.py
@@ -1,11 +1,29 @@
-from flask import Flask
+# import mimetypes
+# from turtle import pd
+from flask import Flask, Response
 from flask_cors import CORS
+import os
+import pandas as pd
 
 app = Flask(__name__)
 
 CORS(app)
 
+# Statt Pfad mit Schrägstrich, oder Rückschlag anzugeben, os.path.join nutzen, da es sonst von
+# einigen Betriebssystemen falsch interperetiert
+training_data = pd.read_csv(os.path.join('data', 'auto-mpg.csv'))
+
 
 @app.route("/", methods=["GET"])
 def index():
-    return{"Hallo": "Erde", "Hi": "Mars", "ähäoü": "Venus"}
+    return{"Hallo": "Erde", "Hi": "Mars", "Ähäoü": "Venus"}
+
+
+@app.route("/hallo_welt", methods=["GET"])
+def hello_world():
+    return "<p>Hallo Welt!</p>"
+
+
+@app.route("/training_data", methods=["GET"])
+def get_training_data():
+    return Response(training_data.to_json(), mimetype='application/json')
